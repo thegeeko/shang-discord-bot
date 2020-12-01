4d2fab13a41feec71555261eaea5cda0d54e6cba
abdelhadi <xxxx7028@gmail.com>
2020-12-01 13:15:04 +0200

    put the api key in an env var

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,30 @@
 import Discord from 'discord.js';
+import dotenv from 'dotenv';
 import path from 'path';
 import fs from 'fs';
+
+dotenv.config();
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client?.user?.tag}!`);
+	console.log(`Logged in as ${client?.user?.tag}!`);
 });
 
-let msgs:any;
+let msgs: any;
 
-client.on('message', msg => {
-  msgs = JSON.parse(fs.readFileSync(path.join(__dirname, "msgsreply.json"),'utf-8'));
-  for(let key in msgs) {
-    if (msg.content === key) {
-      msg.reply(msgs[key]);
-    }
-  }
-  
+client.on('message', (msg) => {
+	if (msg.content.split('/')[0] == 'g') {
+    
+	} else {
+		msgs = JSON.parse(fs.readFileSync(path.join(__dirname, 'msgsreply.json'), 'utf-8'));
+		for (let key in msgs) {
+			if (msg.content === key) {
+				msg.reply(msgs[key]);
+			}
+		}
+	}
 });
 
+console.log(process.env.API_KEY)
 
-
-client.login('NzgzMjY1NDU4Mzc1NDkxNjE0.X8YOzQ.Ij3b9-JV4piaV-b08q9BTC-rouY');
+client.login(process.env.API_KEY).catch(err => console.log(err));
